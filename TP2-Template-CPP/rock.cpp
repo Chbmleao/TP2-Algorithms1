31767beb6dd1774d3924c6f3003c679eb802bcbd
Carlos Leão <caikleao@gmail.com>
2022-11-01 09:34:53 -0300

    Add an interval identifier for the maxSubarraySum function.

--- TP2-Template-CPP/rock.cpp
+++ TP2-Template-CPP/rock.cpp
@@ -4,15 +4,49 @@
 #include <string>
 #include <stdlib.h>
 
+#define SUMSUBARRAY 0
+#define MAXSUBARRAY 1
+#define PRESUBARRAY 2
+#define SUFSUBARRAY 3
+
 Solution::Solution() {
     this->maxSubarraySum = 0;
     this->prefix = 0;
     this->suffix = 0;
     this->sum = 0;
+    this->sumInterval[0] = -1;
+    this->sumInterval[1] = -1;
+    this->maxSubarrayInterval[0] = -1;
+    this->maxSubarrayInterval[1] = -1;
+    this->suffixInterval[0] = -1;
+    this->suffixInterval[1] = -1;
+    this->prefixInterval[0] = -1;
+    this->prefixInterval[1] = -1;
+}
+
+void Solution::updateInterval(int intervalId, int start, int end) {
+    if(start == -1 || end == -1)
+        return;
+    if (intervalId == SUMSUBARRAY) {
+        this->sumInterval[0] = start;
+        this->sumInterval[1] = end;
+    }
+    if (intervalId == MAXSUBARRAY) {
+        this->maxSubarrayInterval[0] = start;
+        this->maxSubarrayInterval[1] = end;
+    }
+    if (intervalId == PRESUBARRAY) {
+        this->prefixInterval[0] = start;
+        this->prefixInterval[1] = end;
+    }
+    if (intervalId == SUFSUBARRAY) {
+        this->suffixInterval[0] = start;
+        this->suffixInterval[1] = end;
+    }
 }
 
 float maxFloat(float x, float y) {
-    if (x >= y)
+    if (x > y)
         return x;
     return y;
 }
@@ -25,6 +59,15 @@ Solution maxSubarraySum(std::vector<float> vector, int start, int end) {
         solution.suffix = maxFloat(0, vector[start]);
         solution.prefix = maxFloat(0, vector[start]);
 
+        solution.updateInterval(SUMSUBARRAY, start, end);
+
+        if(vector[start] >= 0) {
+            solution.updateInterval(MAXSUBARRAY, start, end);
+            solution.updateInterval(SUFSUBARRAY, start, end);
+            solution.updateInterval(PRESUBARRAY, start, end);
+        }
+        
+
         return solution;
     }
 
@@ -36,6 +79,29 @@ Solution maxSubarraySum(std::vector<float> vector, int start, int end) {
                                         leftSolution.suffix + rightSolution.prefix);
     solution.suffix = maxFloat(rightSolution.suffix, rightSolution.sum + leftSolution.suffix);
     solution.prefix = maxFloat(leftSolution.prefix, leftSolution.sum + rightSolution.prefix);
+
+    // find the sum interval
+    solution.updateInterval(SUMSUBARRAY, leftSolution.sumInterval[0], rightSolution.sumInterval[1]);
+
+    // find the max subarray interval
+    if(solution.maxSubarraySum == leftSolution.maxSubarraySum)
+        solution.updateInterval(MAXSUBARRAY, leftSolution.maxSubarrayInterval[0], leftSolution.maxSubarrayInterval[1]);
+    if(solution.maxSubarraySum == rightSolution.maxSubarraySum)
+        solution.updateInterval(MAXSUBARRAY, rightSolution.maxSubarrayInterval[0], rightSolution.maxSubarrayInterval[1]);
+    if(solution.maxSubarraySum == (leftSolution.suffix + rightSolution.prefix))
+        solution.updateInterval(MAXSUBARRAY, leftSolution.suffixInterval[0], rightSolution.prefixInterval[1]);
+
+    // find the suffix interval
+    if(solution.suffix == rightSolution.suffix)
+        solution.updateInterval(SUFSUBARRAY, rightSolution.suffixInterval[0], rightSolution.suffixInterval[1]);
+    if(solution.suffix == (leftSolution.suffix + rightSolution.sum))
+        solution.updateInterval(SUFSUBARRAY, leftSolution.suffixInterval[0], rightSolution.sumInterval[1]);
+
+    // find the prefix interval
+    if(solution.prefix == leftSolution.prefix)
+        solution.updateInterval(PRESUBARRAY, leftSolution.prefixInterval[0], leftSolution.prefixInterval[1]);
+    if(solution.prefix == (leftSolution.sum + rightSolution.prefix))
+        solution.updateInterval(PRESUBARRAY, leftSolution.sumInterval[0], rightSolution.prefixInterval[1]);
 
     return solution;
 }
@@ -78,10 +144,10 @@ void readFile() {
 
         Solution rockSolution = maxSubarraySum(friendsRating, 0, numShows-1);
 
-        std::cout << "Max SubArray Sum: " << rockSolution.maxSubarraySum << std::endl << std::endl;
+        std::cout << "Max SubArray Sum: " << rockSolution.maxSubarraySum << std::endl;
+        std::cout << "Max Interval: " << rockSolution.maxSubarrayInterval[0]+1 << " - " << rockSolution.maxSubarrayInterval[1]+1 << std::endl << std::endl;
 
         std::cin >> numFriends;
         std::cin >> numShows;
-    } ;
-
+    }
 }
--- TP2-Template-CPP/rock.hpp
+++ TP2-Template-CPP/rock.hpp
@@ -4,10 +4,15 @@
 class Solution {
     public:
         Solution();
+        void updateInterval(int intervalId, int start, int end);
         float sum;
         float maxSubarraySum;
         float suffix;
         float prefix;
+        int sumInterval[2];
+        int maxSubarrayInterval[2];
+        int suffixInterval[2];
+        int prefixInterval[2];
 };
 
 std::vector<float> createTotalRatingVector(int numRows, int numCols);
